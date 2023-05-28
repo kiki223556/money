e4780a734f4f7ba5bd59194f20b5a0b89bc2470f
kiki223556 <kiki223556@gmail.com>
2023-05-28 15:44:10 +0800

    修正sidemenu樣式

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,29 +1,27 @@
 <template>
-  <div class="sidemenu-container">
-    <h3>money web</h3>
-    <el-menu
-      default-active="1"
-      text-color="#fff"
-      active-text-color="#F0EB8D"
-      background-color="#8F43EE"
-      class="el-menu-vertical"
-    >
-      <el-menu-item index="1">
-        <el-icon><HomeFilled /></el-icon>
-        <span>首頁</span>
-      </el-menu-item>
-      <el-menu-item index="2">
-        <el-icon><Setting /></el-icon>
-        <span>設定</span>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <el-icon><Document /></el-icon>
-        <span>備註</span>
-      </el-menu-item>
-    </el-menu>
-    <div class="avatar-content">
-      <UserAvatar></UserAvatar>
-    </div>
+  <h3 class="a">money web</h3>
+  <el-menu
+    default-active="1"
+    text-color="#fff"
+    active-text-color="#F0EB8D"
+    background-color="#8F43EE"
+    class="el-menu-vertical a"
+  >
+    <el-menu-item index="1">
+      <el-icon><HomeFilled /></el-icon>
+      <span>首頁</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <el-icon><Setting /></el-icon>
+      <span>設定</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <el-icon><Document /></el-icon>
+      <span>備註</span>
+    </el-menu-item>
+  </el-menu>
+  <div class="a">
+    <UserAvatar></UserAvatar>
   </div>
 </template>
 
@@ -35,16 +33,14 @@ import UserAvatar from "./UserAvatar.vue";
 .el-menu-vertical {
   background-color: transparent;
   overflow-y: scroll;
+  flex-grow: 2;
+  flex-shrink: 1;
+  flex-basis: 0%;
 }
 .el-menu-item {
   font-size: 16px;
 }
-.sidemenu-container {
-  height: 80vh;
-  display: flex;
-  flex-direction: column;
-}
-.avatar-content {
-  height: 400px;
+.a {
+  flex: 0;
 }
 </style>
