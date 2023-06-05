9fc2ca881ae0966f4481018cfc6bd1ed6829558e
kiki223556 <kiki223556@gmail.com>
2023-06-05 18:54:34 +0800

    feat: 添加用户頭像和用户名顯示功能

--- src/components/UserAvatar.vue
+++ src/components/UserAvatar.vue
@@ -1,17 +1,56 @@
+<!-- eslint-disable prettier/prettier -->
 <template>
   <div class="avatar-container">
-    <div>
-      <el-avatar>user</el-avatar>
-      <span>使用者名稱</span>
+    <div class="avatar-align">
+      <el-avatar v-show="!userInfo.avatar" :icon="UserFilled" />
+      <el-avatar v-show="userInfo.avatar" :src="userInfo.avatar" />
+      <span v-show="!userInfo.name">尚未登入</span>
+      <span>{{ userInfo.name }}</span>
     </div>
-    <div class="more-icon">
-      <el-icon><MoreFilled /></el-icon>
-    </div>
+    <el-dropdown>
+      <button class="more-icon">
+        <el-icon><MoreFilled /></el-icon>
+      </button>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <GoogleLogin :callback="callback" />
+          <el-dropdown-item @click="logout" :icon="ArrowLeft">登出</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
   </div>
 </template>
 
+<script lang="ts" setup>
+import { reactive } from "vue";
+import { ArrowLeft, UserFilled } from "@element-plus/icons-vue";
+import { decodeCredential } from "vue3-google-login";
+
+const userInfo = reactive({
+  avatar: "",
+  name: "",
+});
+
+const callback = (response: { credential: string }) => {
+  const userData = decodeCredential(response.credential);
+  console.log("Handle the userData", userData);
+
+  userInfo.avatar = userData.picture;
+  userInfo.name = userData.name;
+};
+
+const logout = () => {
+  console.log("logout");
+};
+</script>
+
 <style scoped>
+.avatar-align {
+  display: flex;
+  align-items: center;
+}
 .avatar-container {
+  border: 0px;
   border-top: 1px solid #fff;
   width: 280px;
   margin: 0 20px;
@@ -20,12 +59,23 @@
   padding: 15px 0px;
   display: flex;
   justify-content: space-between;
+
+  background-color: transparent;
+  color: #fff;
 }
 
 .el-avatar {
   margin-right: 10px;
 }
 .more-icon {
-  margin: 10px 5px 0 0;
+  cursor: pointer;
+  margin: 10px 5px 10px 0;
+  background-color: transparent;
+  border: 0px;
+  color: #fff;
+}
+
+.more-icon:hover {
+  background-color: #67586a;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { createApp } from "vue";
 import router from "./router";
 import ElementPlus from "element-plus";
@@ -5,16 +6,20 @@ import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import { createPinia } from "pinia";
 import "@/styles/index.css";
+import vue3GoogleLogin from "vue3-google-login";
 import App from "./App.vue";
 
 const pinia = createPinia();
 const app = createApp(App);
+app.use(vue3GoogleLogin, {
+  clientId: "183059677215-n8kpfe77hk7e7njus9g5g4qur2fvjc9e.apps.googleusercontent.com",
+});
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
-app.use(router);
 app.use(ElementPlus);
+app.use(router);
 app.use(pinia);
 
 app.mount("#app");
