b88e3b8237c0a1000634274cd47840925a95960b
kiki223556 <kiki223556@gmail.com>
2023-06-07 19:02:17 +0800

    chore: create RecordCard component

---
+++ src/components/Diary/RecordCard.vue
@@ -0,0 +1,96 @@
+<template>
+  <ul class="record-card">
+    <template v-for="group in groupedRecords" :key="group.id">
+      <el-card class="record-box">
+        <template #header>
+          <span>{{ group.date }}</span>
+        </template>
+        <ul class="record-list">
+          <li v-for="record in group.records" :key="record.id" class="record-item">
+            <div class="record-info">
+              <el-button circle size="small" :icon="record.icon" color="#8F43EE" />
+              <span class="record-name">{{ record.name }}</span>
+            </div>
+            <div>
+              <span>$ {{ record.price }} </span>
+              <el-button
+                circle
+                class="record-btn"
+                type="danger"
+                color="#8F43EE"
+                size="small"
+                :icon="Delete"
+                plain
+              />
+              <el-button
+                circle
+                class="record-btn"
+                color="#8F43EE"
+                type="primary"
+                size="small"
+                :icon="Edit"
+                plain
+              />
+            </div>
+          </li>
+        </ul>
+      </el-card>
+    </template>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { Edit, Delete } from "@element-plus/icons-vue";
+defineProps(["groupedRecords"]);
+</script>
+
+<style scoped>
+.record-info {
+  padding-left: 5px;
+}
+li .record-btn {
+  float: right;
+  display: none;
+}
+li:hover .record-btn {
+  display: block;
+  margin-right: 6px;
+}
+
+.record-item:hover {
+  border-radius: 5px;
+  background-color: #a67cda;
+  transition: all 0.2s ease-in-out;
+}
+.record-name {
+  margin-left: 10px;
+}
+.record-box {
+  margin: 10px;
+  width: 500px;
+  color: #fff;
+  background-color: #413543;
+}
+
+.record-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #aeaeae;
+  padding: 6px 0;
+}
+
+.record-item:last-child {
+  border-bottom: 0px;
+}
+
+.record-card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+}
+.record-list {
+  padding-inline-start: 0px;
+}
+</style>
--- src/views/Diary.vue
+++ src/views/Diary.vue
@@ -3,10 +3,9 @@
     <el-button
       @click="showDialog"
       class="show-dialog-btn"
-      :icon="Edit"
+      :icon="Plus"
       circle
       size="large"
-      type="warning"
       color="#F0EB8D"
     />
     <el-dialog
@@ -67,44 +66,7 @@
       </el-form>
     </el-dialog>
 
-    <ul class="record-card">
-      <template v-for="group in groupedRecords" :key="group.id">
-        <el-card class="record-box">
-          <template #header>
-            <span>{{ group.date }}</span>
-          </template>
-          <ul class="record-list">
-            <li v-for="record in group.records" :key="record.id" class="record-item">
-              <div class="record-info">
-                <el-button circle size="small" :icon="record.icon" color="#8F43EE" />
-                <span class="record-name">{{ record.name }}</span>
-              </div>
-              <div>
-                <span>$ {{ record.price }} </span>
-                <el-button
-                  circle
-                  class="record-btn"
-                  type="danger"
-                  color="#8F43EE"
-                  size="small"
-                  :icon="Delete"
-                  plain
-                />
-                <el-button
-                  circle
-                  class="record-btn"
-                  color="#8F43EE"
-                  type="primary"
-                  size="small"
-                  :icon="Edit"
-                  plain
-                />
-              </div>
-            </li>
-          </ul>
-        </el-card>
-      </template>
-    </ul>
+    <RecordCard :groupedRecords="groupedRecords" />
   </div>
 </template>
 
@@ -113,8 +75,9 @@ import { computed, reactive, ref } from "vue";
 import { nanoid } from "nanoid";
 import useDate from "@/hooks/useDate";
 import categories from "@/config/categories";
-import { Edit, Delete } from "@element-plus/icons-vue";
+import { Plus } from "@element-plus/icons-vue";
 import { DiaryRecord } from "@/types/record";
+import RecordCard from "../components/Diary/RecordCard.vue";
 
 let NowDayOfWeek = useDate().NowDayOfWeek;
 
@@ -178,53 +141,16 @@ const groupedRecords = computed(() => {
 
 const dialogVisible = ref(false);
 
-const showDialog = () => {
+function showDialog() {
   dialogVisible.value = true;
-};
+}
 </script>
 
 <style scoped>
-.record-info {
-  padding-left: 5px;
-}
-li .record-btn {
-  float: right;
-  display: none;
-}
-li:hover .record-btn {
-  display: block;
-  margin-right: 6px;
-}
-
-.record-item:hover {
-  background-color: #a67cda;
-  border-radius: 7px;
-}
-.record-name {
-  margin-left: 10px;
-}
-.record-box {
-  margin: 10px;
-  width: 500px;
-  color: #fff;
-  background-color: #413543;
-}
-
 .card-header {
   display: flex;
   justify-content: space-between;
   align-items: center;
-}
-.record-item {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-  padding: 6px 0;
-}
-
-.record-item:last-child {
-  border-bottom: 0px;
 }
 
 .show-dialog-btn {
@@ -232,6 +158,11 @@ li:hover .record-btn {
   display: block;
   top: 30px;
   margin: 30px;
+  border: 0px;
+}
+.show-dialog-btn:hover {
+  border: 0px;
+  background-color: #e1d500;
 }
 .diary-layout {
   height: 100%;
@@ -240,14 +171,5 @@ li:hover .record-btn {
   overflow-x: hidden;
   justify-content: start;
   align-items: center;
-}
-.record-card {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-}
-.record-list {
-  padding-inline-start: 0px;
 }
 </style>
